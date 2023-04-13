318b89bb5b46173af67b5decd76ea7f82cd689cc
svendoejaaren <svendoejaaren@hotmail.com>
2023-04-13 10:43:21 +0200

    Onbelangrijke dingen verwijderd

--- models/user.js
+++
@@ -1,9 +0,0 @@
-const db = require('../server')
-
-const userSchema = {
-    username: { type: String, unique: true, required: true },
-    email: { type: String, unique: true, required: true },
-    password: { type: String, required: true } 
-}
-
-module.exports = userSchema
